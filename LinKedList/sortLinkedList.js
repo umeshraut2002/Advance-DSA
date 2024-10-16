c825182f279ef1b9d0a36fc4c8fb16924aa65ed8
umesh raut <uraut6044@gmail.com>
2024-10-16 11:49:31 +0530

    sort linked list

--- LinKedList/sortLinkedList.js
+++ LinKedList/sortLinkedList.js
@@ -19,15 +19,59 @@ class linkedList{
 
     printList(){
         let temp = this.head;
-        while(temp !==){
+        while(temp !== null){
             console.log(temp.value);
             temp = temp.next;
         }
     }
 
     getHead(){
-        if(!this.head){
-            this.head = 
+        if(this.head === null){
+            console.log("Head : Null");
+        }
+        else{
+            console.log("Head: ", this.head);
         }
     }
+
+    getTail(){
+        if(this.tail === null){
+            console.log("Tail: Null")
+        }
+        else{
+            console.log("Tail : ", this.tail);
+        }
+    }
+
+    makeEmpty(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(value){
+        const newNode = new Node(value);
+
+        if(!this.head){
+            this.head.value = newNode;
+            this.tail =newNode;
+        }
+        else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+    sort(){
+
+    }
 }
+
+const list = new linkedList;
+
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+
+console.log(list)
